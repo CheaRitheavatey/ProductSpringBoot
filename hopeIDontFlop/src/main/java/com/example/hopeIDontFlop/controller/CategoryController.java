bf45379e15e21def52cad3723f4180af9e22778c
Chea Ritheavatey <2023447chea@aupp.edu.kh>
2025-06-26 08:46:26 +0700

    Update jsx

--- hopeIDontFlop/src/main/java/com/example/hopeIDontFlop/controller/CategoryController.java
+++ hopeIDontFlop/src/main/java/com/example/hopeIDontFlop/controller/CategoryController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "api/category")
 @AllArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173/")
+@CrossOrigin(origins = "http://localhost:5173/" )
 public class CategoryController {
     // data field
     private final CategoryService categoryService;
